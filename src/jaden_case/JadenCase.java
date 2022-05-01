dd270e64768b3419554caba11bc6b5b17ee535da
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2022-04-30 20:29:56 -0400

    Se agregan comentarios explicando funcionamiento

--- src/jaden_case/JadenCase.java
+++ src/jaden_case/JadenCase.java
@@ -3,20 +3,29 @@ package jaden_case;
 public class JadenCase {
 
     public String toJadenCase(String phrase) {
-        // TODO put your code below this comment
+
+        //Se maneja la posibilidad que el string sea nulo o vacio
         if (phrase == null || phrase.isEmpty()){
             return null;
         }
+        //Se controla la variable para determinar si son multiples palabras en una frase o si es unica
         if (!phrase.contains(" ")){
+            //Se reemplaza el primer valor enconcrado del caracter en la primera posicion por el mismo pero en mayusculas
             return phrase.replaceFirst( String.valueOf(phrase.charAt(0)),String.valueOf(phrase.charAt(0)).toUpperCase());
         } else {
+            //Se separa la frase para trabajar de manera individual con cada palabra
             String[] splittedPhrase = phrase.split(" ");
+            //Se genera string builder para concatenar el string formado por el arreglo de que compone la palabra
             StringBuilder toReturn = new StringBuilder();
             for (int i = 0; i < splittedPhrase.length; i++){
+                //Se reemplazan los espacios extra en cada palabra para un mejor manejo del resultado
                 String currentWord = splittedPhrase[i].replace(" ", "");
+                //Se reemplaza la palabra en la pocision i con la misma palabra pero con su primer caracter mayuscula
                 splittedPhrase[i] = currentWord.replaceFirst(String.valueOf(currentWord.charAt(0)), String.valueOf(currentWord.charAt(0)).toUpperCase());
+                //Se concatena el resultado en el string builder junto con un espacio para separar palabra
                 toReturn.append(splittedPhrase[i]).append(" ");
             }
+            //Se devuelve el string builder eliminando el espacio extra en la ultima pocision
             return toReturn.deleteCharAt(toReturn.length() - 1).toString();
         }
 
--- src/jaden_case/JadenCaseTest.java
+++ src/jaden_case/JadenCaseTest.java
@@ -2,6 +2,41 @@ package jaden_case;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JadenCaseTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class JadenCaseTest {
+
+
+    JadenCase jadenCase = new JadenCase();
+    /*
+    Se evalua el caso de que el string sea una frase con multiples palabras, en este caso deberia retornar cada palabra con
+    su primera letra mayuscula
+     */
+    @Test
+    public void test() {
+        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", "Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"));
+    }
+
+    /*
+    Se evalua el caso de que la frase sea nula, en este caso el retorno del metodo debe ser igualmente nulo
+     */
+
+    @Test
+    public void testNullArg() {
+        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(null));
+    }
+
+    /*
+    Se evalua el caso de que se pase una frase vacia, en este caso se debe retornar nulo
+     */
+
+    @Test
+    public void testEmptyArg() {
+        assertNull("Must return null when the arg is empty string", jadenCase.toJadenCase(""));
+    }
 
 }
